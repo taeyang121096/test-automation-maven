21f99d7a3c2508b3d8e5468ddeebb7c70ecf9bf1
taeyang121096 <taeyang121096@naver.com>
2025-08-19 21:53:47 +0900

    project init

---
+++ src/main/java/com/example/testautomationmaven/TestAutomationMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.testautomationmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAutomationMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testautomationmaven/TestAutomationMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testautomationmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutomationMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
